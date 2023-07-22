b699c682e6d2f9e345793e1270170f0f176a638e
Soumak Dutta <Soumak.Dutta@dnow.com>
2023-07-22 21:06:54 +0530

    Menu page and search bar changes

--- src/Components/Body.js
+++ src/Components/Body.js
@@ -18,20 +18,21 @@ export const Body = () => {
         setFilteredList(filteredList);
     }
     return listResturant.length === 0 ? <Shimmer/> : (
-        <div className="body">
-            <div className="filter flex">
-                <div className="search p-4 m-4">
-                    <input type="text" className="border-solid py-1 border-black border w-60" value={searchText} onChange={(e) => 
-                    {
-                        if(e.target.value === "") setFilteredList(listResturant);
-                        setsearchText(e.target.value)
-                    }}/>
-                    <button className="bg-green-400 hover:opacity-80 px-4 py-2 m-4 rounded-lg" onClick={searchResturant}>Search</button>
-                </div>
-                <div className="search m-4 p-4 flex items-center">
-                    <button className="bg-red-300 hover:opacity-80 px-4 py-2 m-4 rounded-lg" onClick={topRatedResturants}>
-                        Show top rated resturants.
-                    </button>
+        <div className="container-lg">
+            <div className="w-96 px-[23px]">
+                <div className="relative">
+                    <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+                    <svg className="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
+                        <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
+                    </svg>
+                    </div>
+                <input type="text" id="default-search" className="w-full p-4 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:border-orange-400 focus:ring-orange-400 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search..." value={searchText} onChange={
+                    (e) => {
+                        if(e.target.value === ""){
+                            setFilteredList(listResturant);
+                        }
+                        setsearchText(e.target.value)}}/>
+                <button type="submit" class="text-white absolute right-2.5 bottom-2.5 bg-orange-400 hover:bg-orange-500 focus:ring-4 focus:outline-none focus:ring-orange-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" onClick={searchResturant}> Search</button>
                 </div>
             </div>
             <div className="flex flex-wrap">
--- src/Components/RestaurantMenu.js
+++ src/Components/RestaurantMenu.js
@@ -2,22 +2,37 @@
 import Shimmer from "./Shimmer";
 import { useParams } from "react-router-dom";
 import useRestaurantMenu from "../utils/useRestaurantMenu";
+import { IMAGE_URL } from "../utils/constants";
+import { useEffect, useState } from "react";
 const RestaurantMenu = () => {
     const {resId} = useParams();
     const resInfo = useRestaurantMenu(resId);
     if(resInfo === null || resInfo === undefined) return <Shimmer/>;
     const {name,cuisines,costForTwoMessage} = resInfo?.cards[0]?.card?.card?.info;
-    const {itemCards} = resInfo?.cards[2]?.groupedCard?.cardGroupMap?.REGULAR?.cards[1].card.card;
+    const res = resInfo?.cards[2]?.groupedCard?.cardGroupMap?.REGULAR.cards.filter(p=> p.card.card.title === "Recommended");
+    const {itemCards} = res[0].card.card;
     return (
-        <div className="menu">
-            <h1>{name}</h1>
-            <h3>{cuisines.join(",")}</h3>
-            <h3>{costForTwoMessage}</h3>
-            <ul>
-            {itemCards.map(info => <li key = {info?.card?.info?.id}>
-                {info?.card?.info?.name  + " - Rs. "} 
-                {info?.card?.info?.price/100 || info?.card?.info?.defaultPrice/100}</li>)}
-            </ul>
+        <div className="container">
+            <div className="pl-4">
+                <p className="text-lg font-sans font-bold">{name}</p>
+                <p className="text-sm">{cuisines.join(",")}</p>
+                <h3>{costForTwoMessage}</h3>
+            </div>
+            <div>
+             {itemCards.map(info => (
+                <div key = {info?.card?.info?.id} className="p-2 m-4 w-lg bg-white shadow-lg justify-between flex hover:scale-95">
+                    <div className="p-4">
+                    <p className="font-sans">{info?.card?.info?.name}</p>
+                    <p className="font-sans">Rs. {info?.card?.info?.price/100 || info?.card?.info?.defaultPrice /100}</p>
+                    <p className="font-light font-sans text-sm">{info?.card?.info?.description}</p>
+                    </div>
+                    <div className="p-4">
+                        <img className="w-[110px] rounded-lg" src={IMAGE_URL + info?.card?.info?.imageId}/>
+                    </div>   
+                </div>
+                ))}
+            
+            </div>
         </div>
     )
 }
